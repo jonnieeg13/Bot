e165d69e65a978cdacf7b96f0998b47a5afb0c90
Gerald Kimeu <gmk9115@mavs.uta.edu>
2022-02-23 17:14:26 -0600

    moved Unique filepath information to untracked file

--- syllabusbot/constants.py
+++ syllabusbot/constants.py
@@ -1,3 +1,5 @@
+import syllabusbot.unconstants as uncons
+
 BASE_URL = "https://www.uta.edu/mymav/"
 FRONT_STUDENT_BTN = r"studentBtn"
 LOGIN_BTN = '//button[normalize-space()="Student Login Here >"]'
@@ -7,8 +9,8 @@ PASSWORD_SELECT_ID = r"i0118"
 PASSWORD_SIGNIN_ID = '//input[@type="submit" and @value="Sign in"]'
 MANAGE_CLASSES = r"win0groupletPTNUI_LAND_REC_GROUPLET$5"
 COURSE_TEXT = '//h2[@class="ps_header-group"]/a'
-DRIVER_PATH = r'C:\Users\topse\Desktop\VScode\Bot\syllabusbot\chromedriver_win32''\\'
+DRIVER_PATH = uncons.DRIVER_PATH
 COURSE_TEXTS_PANEL = r'win0divSSR_DER0_CAL_FL_SSR_GRPSFF_FL'
 COURSE_TEXTS_CLASS = r'ps_header-group'
 INDIVIDUAL_COURSE_CLASS = 'ps-link'
-SYLLABUS_FILE_PATH = r"C:\Users\topse\Desktop\VScode"
+SYLLABUS_FILE_PATH = uncons.SYLLABUS_FILE_PATH
