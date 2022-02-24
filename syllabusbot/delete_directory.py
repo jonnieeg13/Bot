cb37d8273029f847ab98a49c6dab9ff8adc67091
Gerald Kimeu <gmk9115@mavs.uta.edu>
2022-02-23 18:36:53 -0600

    formatting

--- syllabusbot/delete_directory.py
+++ syllabusbot/delete_directory.py
@@ -8,5 +8,5 @@ def delete_directory(path):
         print("Error: %s - %s." % (e.filename, e.strerror))
 
 
-file_to_delete = r'C:\Users\jonni\OneDrive\Syllabus\Spring 2025'
+file_to_delete = r''
 delete_directory(file_to_delete)
