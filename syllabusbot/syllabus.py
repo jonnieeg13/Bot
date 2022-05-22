8efc650c2f442023be7fd7a6ea074cc7bd826d38
Gerald Kimeu <gmk9115@mavs.uta.edu>
2022-05-22 15:00:30 -0500

    can return the syllabus links that match the names returned by web scrapper

--- syllabusbot/syllabus.py
+++ syllabusbot/syllabus.py
@@ -1,12 +1,19 @@
 import re
-
 import dotenv
 import os
 import canvasapi
 from html.parser import HTMLParser
 
+myList = ['CSE 3330', 'CSE 3302', 'MATH 3330', 'MATH 2326']
+
 
 class MyHTMLParser(HTMLParser):
+    def __init__(self, class_list=None):
+        if class_list is None:
+            class_list = []
+        self.class_list = class_list
+        super().__init__()
+
     def handle_starttag(self, tag, attrs):
         # Only parse the 'anchor' tag.
         if tag == "a":
@@ -14,31 +21,32 @@ class MyHTMLParser(HTMLParser):
             for name, value in attrs:
                 # If href is defined, print it.
                 # if name == "href":
-                if name == "href" and re.search(r"CSE.3302", value):
-                    print(name, "=", value)
+                for classes in self.class_list:
+                    if name == "href" and re.search(classes, value):
+                        print(name, "=", value)
 
 
-dotenv.load_dotenv(dotenv.find_dotenv())
+def get_syllabus_links(class_list):
 
-TOKEN = os.environ.get('CANVAS_API_TOKEN')
-BASEURL = 'https://uta.instructure.com'
+    dotenv.load_dotenv(dotenv.find_dotenv())
+    token = os.environ.get('CANVAS_API_TOKEN')
+    baseurl = 'https://uta.instructure.com'
+    canvas = canvasapi.Canvas(baseurl, token)
+    user = canvas.get_user('self')
+    paginated_courses = user.get_courses(enrollment_state='active', include=['syllabus_body'])
 
-canvas = canvasapi.Canvas(BASEURL, TOKEN)
+    regex_modified_list = []
+    for course in class_list:
+        regex_modified_list.append(course.replace(' ', '.'))
 
-user = canvas.get_user('self')
+    syl = []
+    for course in paginated_courses:
+        if course.syllabus_body:
+            syl.append(course.syllabus_body)
 
-paginated_courses = user.get_courses(enrollment_state='active', include=['syllabus_body', 'term', 'total_students'])
-# courses = [course.attributes for course in paginated_courses]
-syl = []
-for course in paginated_courses:
-    # print(course.total_students)
-    if course.syllabus_body:
-        # print(course.syllabus_body)
-        syl.append(course.syllabus_body)
+    parser = MyHTMLParser(regex_modified_list)
+    for s in syl:
+        parser.feed(s)
 
-# print(courses[0])
-# print(syl[0])
-# # for string in syl:
-#     print(string)
-parser = MyHTMLParser()
-parser.feed(syl[0])
+
+get_syllabus_links(myList)
