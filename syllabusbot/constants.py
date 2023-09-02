6054b7885a8a95b880969c908e89c36e55c1768e
Gerald Kimeu <gmk9115@mavs.uta.edu>
2023-09-02 12:30:46 -0500

    updated id for MyMav Manage classes elements to enable correct clicking

--- syllabusbot/constants.py
+++ syllabusbot/constants.py
@@ -7,7 +7,7 @@ USER_NAME_SELECT_ID = r"i0116"
 USER_NAME_SELECT_NEXT = r"idSIButton9"
 PASSWORD_SELECT_ID = r"i0118"
 PASSWORD_SIGNIN_ID = '//input[@type="submit" and @value="Sign in"]'
-MANAGE_CLASSES = r"win0groupletPTNUI_LAND_REC_GROUPLET$5"
+MANAGE_CLASSES = r"UTA_HPTILE_MANAGE_CLASSES$3"
 COURSE_TEXT = '//h2[@class="ps_header-group"]/a'
 COURSE_TEXTS_PANEL = r'win0divSSR_DER0_CAL_FL_SSR_GRPSFF_FL'
 COURSE_TEXTS_CLASS = r'ps_header-group'
